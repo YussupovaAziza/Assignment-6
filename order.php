fbdb32b33815f966905439bf547fbe304c839803
Dana-Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2022-12-20 12:57:43 +0600

    Add files via upload

---
+++ order.php
@@ -0,0 +1,23 @@
+<?php
+   include 'connect.php';
+
+   if(isset($_POST["submit"])){
+        $name_c = $_POST["C_name"];
+        $email_c = $_POST["C_email"];
+        $address_c = $_POST["C_address"];
+        $phone_c = $_POST["C_phone"];
+        $selected = $_POST["selection"];
+        $mass_c = $_POST["C_mass"];
+        $time_c = $_POST["time"];
+        $sql = "INSERT INTO `order`(`name`, `email`, `address`, `phone`, `date`, `type`, `mass`)
+         VALUES ('$name_c','$email_c','$address_c','$phone_c','$time_c','$selected','$mass_c')";
+        $result = $conn->query($sql);
+        if(!$result){
+          die('Connection Failed   :  '.$conn->connect_error);
+        }
+        else{
+          header("Location: Order.html");
+        }
+
+   }
+?>
